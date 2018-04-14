6f21acbf9448d97361616b075928dea51d7fdbba
joshcano <joshcano@gmail.com>
2018-04-14 01:21:17 -0700

    Update database py to include time stamps

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+import datetime
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
@@ -7,8 +8,8 @@ Base = declarative_base()
 
 class Dataset(Base):
   __tablename__ = 'dataset'
-  id = column('id', Integer, primary_key=True)
-  name = column('name', String)
+  id = Column('id', Integer, primary_key=True)
+  name = Column('name', String)
   power_productions = relationship('PowerProduction', backref='power_production')
   battery_percentage_states = relationship('BatteryPercentageState', backref='battery_percentage_state')
   power_feed_ins = relationship('PowerFeedIn', backref='power_feed_in')
@@ -18,7 +19,7 @@ class Dataset(Base):
 
 class PowerProduction(Base):
   __tablename__ = 'powerproduction'
-  id = column('id', Integer, primary_key=True)
+  id = Column('id', Integer, primary_key=True)
   timeseriesvalue = Column(Float)
   time = Column(DateTime)
   dataset_id = Column(Integer, ForeignKey('dataset.id'))
@@ -26,7 +27,7 @@ class PowerProduction(Base):
 
 class BatteryPercentageState(Base):
   __tablename__ = 'batterypercentagestate'
-  id = column('id', Integer, primary_key=True)
+  id = Column('id', Integer, primary_key=True)
   timeseriesvalue = Column(Float)
   time = Column(DateTime)
   dataset_id = Column(Integer, ForeignKey('dataset.id'))
@@ -34,7 +35,7 @@ class BatteryPercentageState(Base):
 
 class PowerFeedIn(Base):
   __tablename__ = 'powerfeedin'
-  id = column('id', Integer, primary_key=True)
+  id = Column('id', Integer, primary_key=True)
   timeseriesvalue = Column(Float)
   time = Column(DateTime)
   dataset_id = Column(Integer, ForeignKey('dataset.id'))
@@ -42,7 +43,7 @@ class PowerFeedIn(Base):
 
 class PowerSelfConsumption(Base):
   __tablename__ = 'powerselfconsumption'
-  id = column('id', Integer, primary_key=True)
+  id = Column('id', Integer, primary_key=True)
   timeseriesvalue = Column(Float)
   time = Column(DateTime)
   dataset_id = Column(Integer, ForeignKey('dataset.id'))
@@ -50,7 +51,7 @@ class PowerSelfConsumption(Base):
 
 class PowerPurchased(Base):
   __tablename__ = 'powerpurchased'
-  id = column('id', Integer, primary_key=True)
+  id = Column('id', Integer, primary_key=True)
   timeseriesvalue = Column(Float)
   time = Column(DateTime)
   dataset_id = Column(Integer, ForeignKey('dataset.id'))
@@ -58,7 +59,7 @@ class PowerPurchased(Base):
 
 class PowerConsumption(Base):
   __tablename__ = 'powerconsumption'
-  id = column('id', Integer, primary_key=True)
+  id = Column('id', Integer, primary_key=True)
   timeseriesvalue = Column(Float)
   time = Column(DateTime)
   dataset_id = Column(Integer, ForeignKey('dataset.id'))
