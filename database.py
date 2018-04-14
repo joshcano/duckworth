8a3882b71439d2e0a47824c95f128737bd19793e
joshcano <joshcano@gmail.com>
2018-04-14 01:45:01 -0700

    Fix issue with reading schema

--- database.py
+++ database.py
@@ -11,11 +11,11 @@ class Dataset(Base):
   id = Column('id', Integer, primary_key=True)
   name = Column('name', String)
   power_productions = relationship('PowerProduction', backref='power_production')
-#  battery_percentage_states = relationship('BatteryPercentageState', backref='battery_percentage_state')
-#  power_feed_ins = relationship('PowerFeedIn', backref='power_feed_in')
-#  power_self_consumptions = relationship('PowerSelfConsumption', backref='power_self_consumption')
-#  power_purchaseds = relationship('PowerPurchased', backref='power_purchased')
-#  power_consumptions = relationship('PowerConsumption', backref='power_consumption')
+  battery_percentage_states = relationship('BatteryPercentageState', backref='battery_percentage_state')
+  power_feed_ins = relationship('PowerFeedIn', backref='power_feed_in')
+  power_self_consumptions = relationship('PowerSelfConsumption', backref='power_self_consumption')
+  power_purchaseds = relationship('PowerPurchased', backref='power_purchased')
+  power_consumptions = relationship('PowerConsumption', backref='power_consumption')
 
 class PowerProduction(Base):
   __tablename__ = 'power_production'
@@ -23,47 +23,47 @@ class PowerProduction(Base):
   timeseriesvalue = Column(Float)
   time = Column(DateTime)
   dataset_id = Column(Integer, ForeignKey('dataset.id'))
-  dataset = relationship('Dataset', back_populates='power_production')
+  dataset = relationship('Dataset', back_populates='power_productions')
 
-#class BatteryPercentageState(Base):
-#  __tablename__ = 'battery_percentage_state'
-#  id = Column('id', Integer, primary_key=True)
-#  timeseriesvalue = Column(Float)
-#  time = Column(DateTime)
-#  dataset_id = Column(Integer, ForeignKey('dataset.id'))
-#  dataset = relationship('Dataset', back_populates='battery_percentage_state')
-#
-#class PowerFeedIn(Base):
-#  __tablename__ = 'power_feed_in'
-#  id = Column('id', Integer, primary_key=True)
-#  timeseriesvalue = Column(Float)
-#  time = Column(DateTime)
-#  dataset_id = Column(Integer, ForeignKey('dataset.id'))
-#  dataset = relationship('Dataset', back_populates='power_feed_in')
-#
-#class PowerSelfConsumption(Base):
-#  __tablename__ = 'power_self_consumption'
-#  id = Column('id', Integer, primary_key=True)
-#  timeseriesvalue = Column(Float)
-#  time = Column(DateTime)
-#  dataset_id = Column(Integer, ForeignKey('dataset.id'))
-#  dataset = relationship('Dataset', back_populates='power_self_consumption')
-#
-#class PowerPurchased(Base):
-#  __tablename__ = 'power_purchased'
-#  id = Column('id', Integer, primary_key=True)
-#  timeseriesvalue = Column(Float)
-#  time = Column(DateTime)
-#  dataset_id = Column(Integer, ForeignKey('dataset.id'))
-#  dataset = relationship('Dataset', back_populates='power_purchased')
-#
-#class PowerConsumption(Base):
-#  __tablename__ = 'power_consumption'
-#  id = Column('id', Integer, primary_key=True)
-#  timeseriesvalue = Column(Float)
-#  time = Column(DateTime)
-#  dataset_id = Column(Integer, ForeignKey('dataset.id'))
-#  dataset = relationship('Dataset', back_populates='power_consumption')
+class BatteryPercentageState(Base):
+  __tablename__ = 'battery_percentage_state'
+  id = Column('id', Integer, primary_key=True)
+  timeseriesvalue = Column(Float)
+  time = Column(DateTime)
+  dataset_id = Column(Integer, ForeignKey('dataset.id'))
+  dataset = relationship('Dataset', back_populates='battery_percentage_states')
+
+class PowerFeedIn(Base):
+  __tablename__ = 'power_feed_in'
+  id = Column('id', Integer, primary_key=True)
+  timeseriesvalue = Column(Float)
+  time = Column(DateTime)
+  dataset_id = Column(Integer, ForeignKey('dataset.id'))
+  dataset = relationship('Dataset', back_populates='power_feed_ins')
+
+class PowerSelfConsumption(Base):
+  __tablename__ = 'power_self_consumption'
+  id = Column('id', Integer, primary_key=True)
+  timeseriesvalue = Column(Float)
+  time = Column(DateTime)
+  dataset_id = Column(Integer, ForeignKey('dataset.id'))
+  dataset = relationship('Dataset', back_populates='power_self_consumptions')
+
+class PowerPurchased(Base):
+  __tablename__ = 'power_purchased'
+  id = Column('id', Integer, primary_key=True)
+  timeseriesvalue = Column(Float)
+  time = Column(DateTime)
+  dataset_id = Column(Integer, ForeignKey('dataset.id'))
+  dataset = relationship('Dataset', back_populates='power_purchaseds')
+
+class PowerConsumption(Base):
+  __tablename__ = 'power_consumption'
+  id = Column('id', Integer, primary_key=True)
+  timeseriesvalue = Column(Float)
+  time = Column(DateTime)
+  dataset_id = Column(Integer, ForeignKey('dataset.id'))
+  dataset = relationship('Dataset', back_populates='power_consumptions')
 
 engine = create_engine('sqlite:///duckworth.db', echo=True)
 Base.metadata.create_all(bind=engine)
